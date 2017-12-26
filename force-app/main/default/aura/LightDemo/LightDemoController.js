dd8970ea7760b7eb9882f6a116266020ec9cac3f
GaneshSalesforce <ganesh@prime.com>
2017-12-26 18:35:41 +0530

    component changes

--- force-app/main/default/aura/LightDemo/LightDemoController.js
+++ force-app/main/default/aura/LightDemo/LightDemoController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit : function(component, event) {
+        var action = component.get("c.allContacts");
+        action.setCallback(this,function(result){
+            component.set("v.contacts", result.getReturnValue());
+        });
+        $A.enqueueAction(action);
     }
 })
