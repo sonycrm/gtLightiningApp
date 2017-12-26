60c6380ef12d3709c110039641a520c1fefb27df
GaneshSalesforce <ganesh@prime.com>
2017-12-26 19:16:00 +0530

    done

--- force-app/main/default/aura/LightDemo/LightDemoController.js
+++ force-app/main/default/aura/LightDemo/LightDemoController.js
@@ -1,9 +1,13 @@
 ({
-    doInit : function(component, event) {
+    doInit: function (component, event) {
         var action = component.get("c.allContacts");
         action.setCallback(this,function(result){
             component.set("v.contacts", result.getReturnValue());
         });
         $A.enqueueAction(action);
+    },
+
+    doInitWithHelper: function (component, event, helper) {
+        helper.doFetch(component, event);
     }
 })
--- force-app/main/default/aura/LightDemo/LightDemoHelper.js
+++ force-app/main/default/aura/LightDemo/LightDemoHelper.js
@@ -1,5 +1,9 @@
 ({
-    helperMethod : function() {
-
+    doFetch: function (component, event) {
+        var action = component.get("c.allContacts");
+        action.setCallback(this, function (result) {
+            component.set("v.contacts", result.getReturnValue());
+        });
+        $A.enqueueAction(action);
     }
 })
