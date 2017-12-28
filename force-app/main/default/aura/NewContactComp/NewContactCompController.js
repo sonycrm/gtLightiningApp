895d22f1b4212bb629da7f243c605484376fa115
GaneshSalesforce <ganesh@prime.com>
2017-12-28 17:40:34 +0530

    done

--- force-app/main/default/aura/NewContactComp/NewContactCompController.js
+++ force-app/main/default/aura/NewContactComp/NewContactCompController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
-
+    saveNewContactComp : function(component, event) {
+        var newCont = component.get("v.newContact");
+        var saveAction = component.get("c.saveNewContact");
+        saveAction.setParams({
+            "nCont": newCont
+        });
+        saveAction.setCallback(this,function(result){
+            var res = result.getReturnValue();
+            //alert("Saving object1..." + res);
+            //alert("Saving object2..." + newCont);
+            component.set("v.saveResult",res);
+        });
+        $A.enqueueAction(saveAction);
     }
 })
